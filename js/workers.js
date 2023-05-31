9516fbdd1f273fffe075a4456e46e8f6aee1256d
jasurbek-mannonov <jasurbekmannonov2020@gmail.com>
2023-05-31 19:23:13 +0500

    Workers qismidagi batafsil funksiyasi optimallashtirildi

--- js/workers.js
+++ js/workers.js
@@ -1,14 +1,6 @@
 const url = 'http://95.130.227.52:3010'
 const table = 'worker'
 
-let fname = document.getElementById('name')
-let lname = document.getElementById('lname')
-let email = document.getElementById('email')
-let address = document.getElementById('address')
-let age = document.getElementById('age')
-let phone = document.getElementById('phone')
-let salary = document.getElementById('salary')
-
 let addDep = document.querySelector('#addDep')
 let inputs = document.querySelectorAll('#addDep [name]')
 let tableWorkers = document.querySelector('.table tbody')
@@ -20,7 +12,12 @@ let optionTitles = []; // Array to store option titles
 let workers = []
 let worker = {}
 
- 
+const showModal = new bootstrap.Modal('#showWorker', {
+    keyboard: false
+})
+const showModalToggle = document.getElementById('showWorker')
+
+
 const months = ['Yanvar', 'Fevral', 'Mart', 'Aprel', 'May', 'Iyun', 'Iyul', 'Avgust', 'Sentyabr', 'Oktyabr', 'Noyabr', 'Dekabr']
 
 const addZero = val => val < 10 ? `0${val}` : val
@@ -34,27 +31,26 @@ const parseDate = (date) => {
 // Xodimning qaysi bo'limga mansubligini belgilash uchun
 const getPost = async () => {
     const response = await axios.get("http://95.130.227.52:3010/department");
-    return response.data; 
-  };
-  
-  const displayOption = async () => {
+    return response.data;
+};
+
+const displayOption = async () => {
     const options = await getPost();
     for (const option of options) {
-      const newOption = document.createElement("option");
-      newOption.value = option.title;
-      newOption.text = option.title;
-      workerDepartment.appendChild(newOption);
-      optionTitles.push(option.title);
+        const newOption = document.createElement("option");
+        newOption.value = option._id;
+        newOption.text = option.title;
+        workerDepartment.appendChild(newOption);
+        optionTitles.push(option.title);
     }
-  };
-  
-  const accessOptionTitles = async () => {
+};
+
+const accessOptionTitles = async () => {
     await displayOption();
-    optionTitles.forEach((title) => {
-    });
-  };
-  
-  accessOptionTitles();
+    optionTitles.forEach((title) => {});
+};
+
+accessOptionTitles();
 
 // Renderlash
 const render = (list) => {
@@ -66,14 +62,16 @@ const render = (list) => {
             <td>${item.name}</td>
             <td>${item.lname}</td>
             <td>${item.email}</td>
-            <td>${item.address}</td>
-            <td>${item.age}</td>
-            <td>${parseDate(item.createdTime)}</td>
             <td>${item.phone}</td>
-            <td>${optionTitles[index]}</td>
+            <td>${item.department.title}</td> 
             <td>${item.status == 1 ? 'Faol' : 'Nofaol'}</td>
-            <td>${item.salary}</td>
             <td class="text-end">
+                <button 
+                type="button" 
+                class="btn btn-success"
+                onclick="showWorker('${item._id}')"
+                >
+                <i class="bi bi-eye"></i>
                 <button 
                 type="button" 
                 class="btn btn-danger"
@@ -97,31 +95,24 @@ const getWorkers = async () => {
     let res = await axios.get(`${url}/${table}`)
     if (res.status == 200) {
         workers = res.data
-        
+
     }
     render(workers)
 }
 
 // Qo'shish
 const addWorkers = async () => {
-    let res = await axios.post(`${url}/${table}`,{
-        name: fname.value,
-        lname: lname.value,
-        email: email.value,
-        address: address.value,
-        age: age.value,
-        phone: phone.value,
-        // department: optionTitles,
-        salary: salary.value
+    let worker = {}
+    inputs.forEach(input => {
+        worker[input.getAttribute('name')] = input.value
     })
-    if(res.status == 201){
-       workers = [res.data,...workers]
-       render(workers)
+    console.log(worker)
+    let res = await axios.post(`${url}/${table}`, worker)
+    if (res.status == 201) {
+        workers = [res.data, ...workers]
+        render(workers)
     }
 }
-// console.log(workers)
-
-  
 
 // O'chirish
 const deleteWorker = _id => {
@@ -132,6 +123,45 @@ const deleteWorker = _id => {
                 render(workers)
             })
 
+    }
+}
+
+const showWorker = async (id) => {
+    let res = await axios.get(`${url}/${table}/get/${id}`)
+    if(res.status == 200){
+        let s = res.data
+        let str = `
+        <h3 class="text-center">${s.lname} ${s.name}</h3>
+        <h4 class="text-center">${s.department?.title || "Bo'lim qayd etilmagan!"}</h4>
+        <table class="table">
+          <tr>
+              <td>Yosh</td>
+              <td class="text-end">${s.age}</td>
+          </tr>
+          <tr>
+              <td>Telefon raqam:</td>
+              <td class="text-end">${s.phone}</td>
+          </tr>
+          <tr>
+              <td>Email:</td>
+              <td class="text-end">${s.email}</td>
+          </tr>
+          <tr>
+              <td>Manzil:</td>
+              <td class="text-end">${s.address}</td>
+          </tr>
+          <tr>
+              <td>Ishga kelgan vaqt:</td>
+              <td class="text-end">${parseDate(s.createdTime)}</td>
+          </tr>
+          <tr>
+              <td>Maosh:</td>
+              <td class="text-end">${s.salary}</td>
+          </tr>
+        </table>
+        `;
+        document.querySelector('#showWorker .modal-body').innerHTML = str
+        showModal.show(showModalToggle)
     }
 }
 
